e010d19ed9ebba79e9574b775ec2a155f8aa3fb5
Refky <shiejacko@gmail.com>
2025-12-09 12:09:23 +0700

    update code app.sh

--- app.sh
+++ app.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
 
-# declare variable 
+# deklarasi variabel array
+declare -a todo_list
+declare -a task_name
+declare -a task_status # untuk pending/done
+
+# function untuk menampilan daftar tugas
+display_todo() {
+   if [ ${#todo_list[@]} -eq 0 ]; then
+      echo "Tidak ada tugas."
+      return
+   fi
+
+   echo "------ List Tugas ------"
+
+   for i in "${!todo_list[@]}"; do
+      echo "(( i+1 )). ${todo_list[$i]}"
+   done
+
+   echo "------------------------"
+}
+
+# function tambah tugas
+add_task() {
+   # untuk input nama tugas dengan validasi kosong dan duplikasi nama
+   while true; do
+      read -p "Masukan tugas baru: " task
+      if [[ -n "$task" ]]; then
+         task_name += ("$task")
+         task_status += ("Pending")
+         todo_list += ("$task - Pending")
+         echo ""
